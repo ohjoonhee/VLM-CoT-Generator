d8712db845cc7ac6f8237016583e3d5490a9640f
ohjoonhee <ohjoonhee@snu.ac.kr>
2025-12-07 16:01:26 +0900

    feat: Add SLURM script to reassemble and extract split tar archives.

---
+++ scripts/slurm/unzip_tar_split.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J launch               # Job name
+#SBATCH -p cpu-farm               # Partition name
+#SBATCH --qos=normal      # Quality of Service
+#SBATCH --nodes=1                 # Number of nodes
+#SBATCH --ntasks-per-node=1       # Number of tasks (processes) per node
+#SBATCH --cpus-per-task=8         # Number of CPU cores per task
+#SBATCH -o logs/%x_%j.log         # Standard output file path
+
+cd data_viscot/cot_images_tar_split
+
+echo "Working directory: $(pwd)"
+
+cat cot_images_* | tar x
+
+echo "Done"
