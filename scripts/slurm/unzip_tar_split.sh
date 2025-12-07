7c915fac017be65467698035b71fba09af9d56ac
ohjoonhee <ohjoonhee@snu.ac.kr>
2025-12-07 20:39:06 +0900

    perf: increase Slurm cpus-per-task from 8 to 32

--- scripts/slurm/unzip_tar_split.sh
+++ scripts/slurm/unzip_tar_split.sh
@@ -4,7 +4,7 @@
 #SBATCH --qos=normal      # Quality of Service
 #SBATCH --nodes=1                 # Number of nodes
 #SBATCH --ntasks-per-node=1       # Number of tasks (processes) per node
-#SBATCH --cpus-per-task=8         # Number of CPU cores per task
+#SBATCH --cpus-per-task=32         # Number of CPU cores per task
 #SBATCH -o logs/%x_%j.log         # Standard output file path
 
 cd data_viscot/cot_images_tar_split
