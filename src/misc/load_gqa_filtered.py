8188d215373bda3ce2b94cf61a2686167fa9b0f7
ohjoonhee <ohjoonhee@snu.ac.kr>
2025-12-21 18:09:17 +0900

    feat: Implement Firework API client for VLM inference and add dataset preprocessing for multi-image inputs.

--- src/misc/load_gqa_filtered.py
+++ src/misc/load_gqa_filtered.py
@@ -20,7 +20,7 @@ def process_example(example):
 def main():
     cpus = int(os.environ.get("SLURM_CPUS_PER_TASK", os.cpu_count()))
     print("CPU Count: ", cpus)
-    ds = load_dataset("json", data_files="data/Visual-CoT/gqa_cot_train_filtered_heuristic.jsonl", split="train")
+    ds = load_dataset("json", data_files="data_viscot/gqa_cot_train_filtered_heuristic.jsonl", split="train")
     # ds = ds.select(range(100))
     print(ds)
     print(ds[0])
